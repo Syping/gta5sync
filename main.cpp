3ddf4299fc0bc16549ed498c2febc6b56f83186f
Syping <schiedelrafael@keppe.org>
2017-03-31 06:25:58 +0200

    dpi awareness update

--- main.cpp
+++ main.cpp
@@ -49,9 +49,6 @@
 
 int main(int argc, char *argv[])
 {
-#if QT_VERSION >= 0x050600
-    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
     QApplication a(argc, argv);
     a.setApplicationName(GTA5SYNC_APPSTR);
     a.setApplicationVersion(GTA5SYNC_APPVER);
