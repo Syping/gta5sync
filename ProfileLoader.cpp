87ded26eabdf1c9219b77e37e596c49a80fc8ca7
Syping <schiedelrafael@keppe.org>
2017-10-12 19:17:09 +0200

    QDir::seperator replaced

--- ProfileLoader.cpp
+++ ProfileLoader.cpp
@@ -61,7 +61,7 @@ void ProfileLoader::run()
     foreach(const QString &SavegameFile, SavegameFiles)
     {
         emit loadingProgress(curFile, maximumV);
-        QString sgdPath = profileFolder % QDir::separator() % SavegameFile;
+        QString sgdPath = profileFolder % "/" % SavegameFile;
         SavegameData *savegame = new SavegameData(sgdPath);
         if (savegame->readingSavegame())
         {
@@ -72,7 +72,7 @@ void ProfileLoader::run()
     foreach(const QString &SnapmaticPic, SnapmaticPics)
     {
         emit loadingProgress(curFile, maximumV);
-        QString picturePath = profileFolder % QDir::separator() % SnapmaticPic;
+        QString picturePath = profileFolder % "/" % SnapmaticPic;
         SnapmaticPicture *picture = new SnapmaticPicture(picturePath);
         if (picture->readingPicture(true, true, true))
         {
