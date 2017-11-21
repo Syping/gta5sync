d8eb7c074cedfba4de6110e7ded73c83162884f9
Syping <schiedelrafael@keppe.org>
2017-11-21 08:04:23 +0100

    add icon to first OptionsDialog

--- main.cpp
+++ main.cpp
@@ -110,6 +110,7 @@ int main(int argc, char *argv[])
         {
             ProfileDatabase profileDB;
             OptionsDialog optionsDialog(&profileDB);
+            optionsDialog.setWindowIcon(IconLoader::loadingAppIcon());
             optionsDialog.show();
             optionsDialog.exec();
         }
