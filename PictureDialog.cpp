6a28988b9adff10605fa0f994268daadffdb0f34
Syping <schiedelrafael@keppe.org>
2017-12-07 02:03:31 +0100

    usable again

--- PictureDialog.cpp
+++ PictureDialog.cpp
@@ -257,7 +257,6 @@ bool PictureDialog::nativeEvent(const QByteArray &eventType, void *message, long
             lRet = HitTestNCA(hWnd, msg->lParam);
             DwmDefWindowProc(hWnd, msg->message, msg->wParam, msg->lParam, &lRet);
             *result = lRet;
-            return true;
             return QWidget::nativeEvent(eventType, message, result);
         }
         else
