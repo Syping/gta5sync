21cc4e9b9972a503538262bd0efe4461b7e2847e
Syping <schiedelrafael@keppe.org>
2018-01-14 19:08:05 +0100

    improve PlayerListDialog and SnapmaticEditor

--- SnapmaticEditor.cpp
+++ SnapmaticEditor.cpp
@@ -240,9 +240,9 @@ void SnapmaticEditor::setSnapmaticPlayers(const QStringList &players)
         ui->labPlayers->setText(playersStr.arg(QApplication::translate("PictureDialog", "No Players"), editStr));
     }
 #ifndef Q_OS_ANDROID
-    ui->gbValues->resize(ui->gbValues->sizeHint());
-    ui->frameWidget->resize(ui->frameWidget->sizeHint());
-    resize(width(), heightForWidth(width()));
+    ui->gbValues->resize(ui->gbValues->width(), ui->gbValues->heightForWidth(ui->gbValues->width()));
+    ui->frameWidget->resize(ui->gbValues->width(), ui->frameWidget->heightForWidth(ui->frameWidget->width()));
+    if (heightForWidth(width()) > height()) { resize(width(), heightForWidth(width())); }
 #endif
 }
 
@@ -268,9 +268,9 @@ void SnapmaticEditor::setSnapmaticTitle(const QString &title)
         ui->labAppropriate->setText(tr("Appropriate: %1").arg(QString("<span style=\"color: red\">%1</a>").arg(tr("No", "No, could lead to issues"))));
     }
 #ifndef Q_OS_ANDROID
-    ui->gbValues->resize(ui->gbValues->sizeHint());
-    ui->frameWidget->resize(ui->frameWidget->sizeHint());
-    resize(width(), heightForWidth(width()));
+    ui->gbValues->resize(ui->gbValues->width(), ui->gbValues->heightForWidth(ui->gbValues->width()));
+    ui->frameWidget->resize(ui->gbValues->width(), ui->frameWidget->heightForWidth(ui->frameWidget->width()));
+    if (heightForWidth(width()) > height()) { resize(width(), heightForWidth(width())); }
 #endif
 }
 
@@ -280,9 +280,9 @@ void SnapmaticEditor::setSnapmaticCrew(const QString &crew)
     QString crewStr = tr("Crew: %1 (%2)").arg(StringParser::escapeString(crew), editStr);
     ui->labCrew->setText(crewStr);
 #ifndef Q_OS_ANDROID
-    ui->gbValues->resize(ui->gbValues->sizeHint());
-    ui->frameWidget->resize(ui->frameWidget->sizeHint());
-    resize(width(), heightForWidth(width()));
+    ui->gbValues->resize(ui->gbValues->width(), ui->gbValues->heightForWidth(ui->gbValues->width()));
+    ui->frameWidget->resize(ui->gbValues->width(), ui->frameWidget->heightForWidth(ui->frameWidget->width()));
+    if (heightForWidth(width()) > height()) { resize(width(), heightForWidth(width())); }
 #endif
 }
 
