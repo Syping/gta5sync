7109e1dcab0bb5a6b132997e479fc2284939052b
Syping <schiedelrafael@keppe.org>
2018-03-06 09:12:23 +0100

    JSHighlighter copyright notice updated

--- anpro/JSHighlighter.cpp
+++ anpro/JSHighlighter.cpp
@@ -1,6 +1,6 @@
 /*****************************************************************************
 * gta5sync GRAND THEFT AUTO V SYNC
-* Copyright (C) 2017 Syping
+* Copyright (C) 2017-2018 Syping
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
